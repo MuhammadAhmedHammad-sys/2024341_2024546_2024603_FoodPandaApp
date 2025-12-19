a7ae828ac7e12a7d8139d3641cdd127e2f20797e
Abidkazmi7 <abidkazmi777@gmail.com>
2025-12-19 21:27:16 +0500

    Restaurant sign-in/log-in functionality

--- restaurant.h
+++ restaurant.h
@@ -3,30 +3,75 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 class Restaurant
 {
 private:
     string filename;
-    string adminUsername;
-    string adminPassword;
+    string managerUsername;
+    string managerPassword;
     bool verified=false;
 
 public:
-    Restaurant()
+    Restaurant(string file = "restaurant/restaurant_credentials.txt")
     {
-
+        this->filename = file;
     }
 
-    bool verifyCredentials(string username, string password)
+    bool login(string username, string password)
     {
-        if (username == adminUsername && password == adminPassword)
+        ifstream fileHandle(filename);
+        string temp_username, temp_password;
+        while (getline(fileHandle, temp_username) && getline(fileHandle, temp_password))
         {
-            verified = true;
+            if (temp_username == username && temp_password == password)
+            {
+                this->managerUsername = temp_username;
+                this->managerPassword = temp_password;
+                this->verified=true;
+                fileHandle.close();
+                return true;
+            }
+        }
+        fileHandle.close();
+
+        cerr << "Username and Password do not match" << endl;
+        return false;
+    }
+
+    bool signup(string username, string password){
+        ifstream fileHandle(filename);
+        string temp_username, temp_password;
+        while (getline(fileHandle, temp_username))
+        {
+            if (temp_username == username)
+            {
+                fileHandle.close();
+                cerr <<"Username already exists. Please choose a different username." << endl;
+                return false;    
+            }
+        }
+        fileHandle.close();
+
+        ofstream outFile(filename, ios::app);
+        if (outFile.is_open())
+        {
+            outFile << username << endl;
+            outFile << password << endl;
+            outFile.close();
+            cout << "Signup successful! You can now log in." << endl;
+
+            this->managerUsername = username;
+            this->managerPassword = password;
+            this->verified = true;
+
             return true;
         }
-        return false;
+        if (!outFile.is_open())
+            cerr << "Error: Unable to open file for writing." << endl;
+        return false;    
     }
 
     void restaurantWindow()
@@ -34,62 +79,51 @@ public:
         cout << "========= Restaurant Panel =========" << endl;
         if (!verified)
         {
-            string username, password;
-            cout << "Enter Manager Username: ";
-            cin >> username;
-            cout << "Enter Manager Password: ";
-            cin >> password;
-
-            if (!verifyCredentials(username, password))
-            {
-                cout << "Invalid credentials. Access denied." << endl;
-                return;
-            }
-        }
-
-        while (true)
-        {
-            cout << "Welcome, Chef!" << endl
-                 << "Loading Data..." << endl
-                 << endl;
-
+            cout << "Login (1) / Signup (2): ";
             int choice;
-            cout << "Menu:" << endl;
-            cout << "\t1. View Menu" << endl;
-            cout << "\t2. Add Item" << endl;
-            cout << "\t3. Remove Item" << endl;
-            cout << "\t4. Save and Exit Restaurant Panel" << endl;
-            cout << "\t5. Change Restaurant Credentials" << endl;
-            cout << "Enter your choice: ";
             cin >> choice;
+            string username, password;
             switch (choice)
             {
             case 1:
-
+            {
+                cout << "Enter Username: ";
+                cin >> username;
+                cout << "Enter Password: ";
+                cin >> password;
+                if (!login(username, password))
+                {
+                    cout << "Login failed. Please try again." << endl;
+                    return;
+                }
                 break;
+            }
             case 2:
             {
-
-                break;
-            }
-            case 3:
-            {
-
-                break;
-            }
-            case 4:
- 
-                cout << "Data saved. Exiting Restaurant Panel." << endl;
-                return;
-            case 5:
-            {
-
+                cout << "Enter Username: ";
+                cin >> username;
+                cout << "Enter Password: ";
+                cin >> password;
+                if (!signup(username, password))
+                {
+                    return;
+                }
                 break;
             }
             default:
                 cout << "Invalid choice. Please try again." << endl;
+                break;
             }
         }
+
+        // while (verified)
+        // {
+        //     cout << "Welcome, " << this->managerUsername << "!" << endl
+        //          << "Loading Data..." << endl
+        //          << endl;
+
+        // }
+        
     }
 };
 
